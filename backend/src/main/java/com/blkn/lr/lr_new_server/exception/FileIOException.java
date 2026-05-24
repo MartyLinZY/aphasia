89e1f0344187ba31cbad0209d3fa6da5d683db55
CZH_Andy <CZH_Andy@noreply.gitcode.com>
2026-05-24 19:12:51 +0800

    exception: 删除 5 个 File*Exception 中 25 处 IDE 自动生成的空 TODO 注释

--- backend/src/main/java/com/blkn/lr/lr_new_server/exception/FileIOException.java
+++ backend/src/main/java/com/blkn/lr/lr_new_server/exception/FileIOException.java
@@ -6,27 +6,22 @@ public class FileIOException extends FileUploadException {
 
 	public FileIOException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileIOException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileIOException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileIOException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileIOException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 	
 	
--- backend/src/main/java/com/blkn/lr/lr_new_server/exception/FileStateException.java
+++ backend/src/main/java/com/blkn/lr/lr_new_server/exception/FileStateException.java
@@ -6,27 +6,22 @@ public class FileStateException extends FileUploadException {
 
 	public FileStateException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileStateException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileStateException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileStateException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public FileStateException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 	
 	
